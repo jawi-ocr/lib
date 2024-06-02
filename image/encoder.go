c64f516ea9543b785950185c8db1e94a7a7c6faa
mushoffa <ridwan.mushoffa@gmail.com>
2024-06-02 11:07:37 +0700

    image: New base64 png image encoder

---
+++ image/encoder.go
@@ -0,0 +1,22 @@
+package image
+
+import(
+	"bytes"
+	"encoding/base64"
+	"log/slog"
+	"image"
+	"image/png"
+)
+
+func Encode(img image.Image) (string, error) {
+	buffer := new(bytes.Buffer)
+
+	if err := png.Encode(buffer, img); err != nil {
+		slog.Error("jawi-ocr/lib", "package", "image", "function", "Encode")
+		return "", err
+	}
+
+	encoded := base64.StdEncoding.EncodeToString(buffer.Bytes())
+
+	return encoded, nil
+}
